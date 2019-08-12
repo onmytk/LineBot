5223b5752a659174174ec75f1adf3fb4437533dc
onmytk <onmytk@gmail.com>
2019-08-13 01:24:08 +0900

    add sqlalchemy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+from flask_sqlalchemy import SQLAlchemy
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -21,6 +22,17 @@ CHANNEL_SECRET = os.environ['CHANNEL_SECRET']
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
+
+db_uri = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+db = SQLAlchemy(app)
+
+
+class Account(db.Model):
+    __tablename__ = 'accounts'
+    date = db.Column(db.Date, nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    type = db.Column(db.String(), nullable=False)
 
 
 # Webhook from LINE
@@ -46,10 +58,17 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        # TextSendMessage(text = os.environ[Response.getResponse(event.message.text)])
         TextSendMessage(text = event.message.text)
     )
 
+def add_account():
+    pass
+
+def delete_account():
+    pass
+
+def get_summary():
+    pass
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
